82aa0ba09f72a1ecc0a8a33dc0ab5533296b90fa
dantaylrr <dannyyyyt@gmail.com>
2021-01-17 16:22:14 +0000

    ammending the scroll threshold

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 100) {
+    if (scroll >= 600) {
         $(".nav-text").addClass("scrolling");
     } else {
         $(".nav-text").removeClass("scrolling");
     }
-    if (scroll >= 100) {
+    if (scroll >= 600) {
         $(".nav-bar").addClass("scrolling");
     } else {
         $(".nav-bar").removeClass("scrolling");
     }
-    if (scroll >= 100) {
+    if (scroll >= 600) {
         $(".nav-links").addClass("scrolling");
     } else {
         $(".nav-links").removeClass("scrolling");
