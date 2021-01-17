bd6d9d5c6813e79e2c6aa478890f1ad4abbd1348
dantaylrr <dannyyyyt@gmail.com>
2021-01-17 17:02:27 +0000

    finally fixed nav-bar scroll that i am happy with across all viewports

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
-    if (scroll >= 600) {
+    if (scroll >= $(window).height()) {
         $(".image").addClass("scrolling");
     } else {
         $(".image").removeClass("scrolling");
     }
-    if (scroll >= 600) {
+    if (scroll >= $(window).height()*0.94) {
         $(".nav-text").addClass("scrolling");
     } else {
         $(".nav-text").removeClass("scrolling");
     }
-    if (scroll >= 600) {
+    if (scroll >= $(window).height()) {
         $(".nav-bar").addClass("scrolling");
     } else {
         $(".nav-bar").removeClass("scrolling");
     }
-    if (scroll >= 600) {
+    if (scroll >= $(window).height()) {
         $(".nav-links").addClass("scrolling");
     } else {
         $(".nav-links").removeClass("scrolling");
